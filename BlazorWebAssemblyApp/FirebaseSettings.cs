bcdc1e6ecb0e187eff82abdc7009c044cf223299
motoki <krkrmfmf@outlook.jp>
2023-01-31 03:20:32 +0900

    コメント追加

--- BlazorWebAssemblyApp/FirebaseSettings.cs
+++ BlazorWebAssemblyApp/FirebaseSettings.cs
@@ -1,9 +1,18 @@
 ﻿namespace BlazorWebAssemblyApp
 {
+    /// <summary>
+    /// Firebase設定情報のモデルです。
+    /// </summary>
     public class FirebaseSettings
     {
+        /// <summary>
+        /// APIキーを取得/設定します。
+        /// </summary>
         public string? FirebaseApiKey { get; set; }
 
+        /// <summary>
+        /// ドメインを取得/設定します。
+        /// </summary>
         public string? FirebaseAuthDomain { get; set; }
 
         public FirebaseSettings()
