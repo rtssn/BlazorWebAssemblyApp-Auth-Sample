c4b50187614f409c092b1be605e28ef3a4bf9f05
Motoki Nakagawa <krkrmfmf@outlook.jp>
2023-02-01 02:24:01 +0900

    コメントを追加

--- BlazorWebAssemblyApp/CustomAuthProvider.cs
+++ BlazorWebAssemblyApp/CustomAuthProvider.cs
@@ -5,16 +5,31 @@ namespace BlazorWebAssemblyApp
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
+        /// <summary>
+        /// 未認証ステータスです。
+        /// </summary>
         private static readonly AuthenticationState UnauthorizedAuthenticationState = new AuthenticationState(new ClaimsPrincipal());
+
+        /// <summary>
+        /// 認証情報です。
+        /// </summary>
         private ClaimsPrincipal? _principal;
 
+        /// <summary>
+        /// 認証状態を返します。
+        /// </summary>
+        /// <returns>認証状態を返します。</returns>
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             if (_principal is null) return Task.FromResult(UnauthorizedAuthenticationState);
             return Task.FromResult(new AuthenticationState(_principal));
         }
 
-        // これ呼んでサインインしてもらう
+        /// <summary>
+        /// ステータスの更新を行います。
+        /// </summary>
+        /// <param name="principal">認証情報を指定します。</param>
+        /// <returns></returns>
         public Task UpdateSignInStatusAsync(ClaimsPrincipal? principal)
         {
             _principal = principal;
